776095ab9d0eb52e434fd7a686d747fef171eef4
Madhur Shrimal <madhur.shrimal@gmail.com>
2024-11-15 00:46:17 +0700

    chore: remove unused code

--- wordlists/wordlists.go
+++ wordlists/wordlists.go
@@ -3,23 +3,11 @@ package wordlists
 import (
 	"embed"
 	"fmt"
-	"path/filepath"
-	"runtime"
 	"strings"
 )
 
 //go:embed all:files/*
 var staticFiles embed.FS
-
-func GetWordListFolderPath() string {
-	_, filename, _, ok := runtime.Caller(0)
-	if !ok {
-		fmt.Println("Unable to get current file path")
-		return ""
-	}
-
-	return filepath.Dir(filename)
-}
 
 func GetWordList(language string) ([]string, error) {
 	data, err := staticFiles.ReadFile("files/" + fmt.Sprintf("%s.txt", language))
